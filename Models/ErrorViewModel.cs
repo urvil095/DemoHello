c892103355082ffecaa125aa44ee82f9824cbd27
urvil095 <urvil095@yahoo.com>
2024-03-01 16:11:52 +0530

    Initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoHello.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
